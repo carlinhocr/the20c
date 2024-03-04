b627bd4df7e2c2ca10807690dfc806713c775a87
carlinhocr <carlos.rienzi@gmail.com>
2024-03-04 14:55:10 -0300

    Two 7Segment Leds

--- TwoBars_TwoLedsDisplay_Arduino/TwoBars_TwoLedsDisplay_Arduino.ino
+++ TwoBars_TwoLedsDisplay_Arduino/TwoBars_TwoLedsDisplay_Arduino.ino
@@ -78,36 +78,14 @@ void decodeNibble(byte lowLed, byte highLed,byte BarValue[4]){
   delay(10);
 }
 void loop() {
-  // reade the bus
   //testAllNumberDual7Segment();
+  // read the bus
   for (int n=0;n<4;n++){
     BarValueHigh[n] = digitalRead(BUSHigh[n]) ? 1:0;
     BarValueLow[n] = digitalRead(BUSLow[n]) ? 1:0;
   };
   decodeNibble(groundLed1,groundLed2,BarValueHigh);
   decodeNibble(groundLed2,groundLed1,BarValueLow);
-  // digitalWrite(groundLed1,LOW);
-  // digitalWrite(groundLed2,HIGH);
-  // light7segment(BarValue,d_0,b_0);
-  // light7segment(BarValue,d_1,b_1);
-  // light7segment(BarValue,d_2,b_2);
-  // light7segment(BarValue,d_3,b_3);
-  // light7segment(BarValue,d_4,b_4);
-  // light7segment(BarValue,d_5,b_5);
-  // light7segment(BarValue,d_6,b_6);
-  // light7segment(BarValue,d_7,b_7);
-  // light7segment(BarValue,d_8,b_8);
-  // light7segment(BarValue,d_9,b_9);
-  // light7segment(BarValue,d_a,b_a);
-  // light7segment(BarValue,d_b,b_b);
-  // light7segment(BarValue,d_c,b_c);
-  // light7segment(BarValue,d_d,b_d);
-  // light7segment(BarValue,d_e,b_e);
-  // light7segment(BarValue,d_f,b_f);
-  // delay(5000);
-  // digitalWrite(groundLed1,HIGH);
-  // digitalWrite(groundLed2,LOW);
-  // delay(5000);
 }
 
 void testNumber(char d_x[7]){
