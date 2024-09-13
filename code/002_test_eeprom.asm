1a91fe5a5cd90d2e02da2fdd7bd7baaf3d85ea44
carlinhocr <carlos.rienzi@gmail.com>
2024-09-13 15:10:28 -0300

    First Commit Arduino,eeprom,ram

---
+++ code/002_test_eeprom.asm
@@ -0,0 +1,19 @@
+
+
+;test simpre instructions in an epprom
+
+  .org $8000
+
+RESET:
+  ;initialize stack
+  lda #$42
+  jmp RESET
+  
+
+;complete the file
+  .org $fffa
+  .word RESET ;a word is 16 bits or two bytes in this case $fffa and $fffb
+  .org $fffc ;go to memory address $fffc of the reset vector
+  .word RESET ;store in $FFFC & $FFFD the memory address of the RESET: label  00 80 ($8000 in little endian)
+  .org $fffe
+  .word RESET ;a word is 16 bits or two bytes in this case $fffe and $ffff
