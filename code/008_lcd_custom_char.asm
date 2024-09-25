9117701433de39b7e7250cd0664babcd28726bb1
carlinhocr <carlos.rienzi@gmail.com>
2024-09-25 12:04:09 -0300

    008 new two chars

--- code/008_lcd_custom_char.asm
+++ code/008_lcd_custom_char.asm
@@ -14,6 +14,10 @@ DDRA = $6003
 startRAMData =$2000
 charDataVectorLow = $30
 charDataVectorHigh = $31
+delay_COUNT_A = $32        
+delay_COUNT_B = $33
+charLoadLow=$28
+charLoadHigh=$29
 
 ;define LCD signals
 E = %10000000 ;Enable Signal
@@ -38,34 +42,7 @@ RESET:
   lda #%11100000  ;set the last 3 pins as output
   sta DDRA ;store the accumulator in the data direction register for Port A
   ;END Initialize LCD Display
-
-  ;BEGIN Add custom char instruction
-  ;8026 lda
-  lda #$40;+#position_custom_char ;the instruction itself is 0001, write a custom character cgram
-  ;or set cg ram address charter positions are 00,08,10,18,20,28,30,38
-  ;8028 jumps to send intrsuction
-  jsr lcd_send_instruction
-  ; END Entry Mode Set instruction
-  ;BEGIN send data for custom character 5x8 char
-  ;a little plane 0, 0x4, 0x4, 0xe, 0x1f, 0x4, 0xe, 0
-  lda #$00
-  jsr lcd_send_data
-  lda #$04
-  jsr lcd_send_data
-  lda #$04
-  jsr lcd_send_data
-  lda #$0e
-  jsr lcd_send_data
-  lda #$1f
-  jsr lcd_send_data
-  lda #$04
-  jsr lcd_send_data
-  lda #$0e
-  jsr lcd_send_data
-  lda #$0
-  jsr lcd_send_data
-  ;END send data for custom character 5x8 char
-
+  jsr add_custom_chars
 
   ; BEGIN clear display instruction  on port B
   lda #%00000001 ;the instruction itself is 00000001
@@ -75,11 +52,11 @@ RESET:
   ; BEGIN send the instruction function set on port B
   lda #%00111000 ;the instruction itself is 001, data lenght 8bits(1), Number Display lines 2 (1)
             ;and Character Font 5x8 (0), last two bits are unused
-  jsr lcd_send_instruction
+  jsr lcd_send_instruction 
   ; END send the instruction function set on port B
 
   ;BEGIN Turn on Display instruction
-  lda #%00001110 ;the instruction itself is 0001, Display On(1), Cursor On (1)
+  lda #%00001100 ;the instruction itself is 0001, Display On(1), Cursor Off (0)
             ;and Cursor Blinking Off (0)
   jsr lcd_send_instruction 
   ; END Turn on Display instruction
@@ -89,16 +66,47 @@ RESET:
             ;and Scroll Display Off (0)
   jsr lcd_send_instruction
   ; END Entry Mode Set instruction
-  lda #"A" ;8022
-  jsr print_char;after 80a1 write the A to port 6000 41 in hexa 
-  ;BEGIN display custom character
+  
+
+write_Screens
+data_low= $00
+data_high= $d0
+;Draw char1
+  lda #$9E 
+  jsr lcd_send_instruction
   lda #$00
-  jsr print_char ;this prints the char and increments DDRAM 
-  ;END display custom character
+  jsr print_char
+  jsr DELAY_HALF_SEC
+;Enter new custome character on char1
+  jsr add_custom_chars_2
+  ;BEGIN Entry Mode Set instruction
+  lda #%00000110 ;the instruction itself is 00001, Put next character to the right (1)
+            ;and Scroll Display Off (0)
+  jsr lcd_send_instruction
+  ; END Entry Mode Set instruction
+;Draw char2
+  lda #$9F 
+  jsr lcd_send_instruction
+  lda #$00
+  jsr print_char
+  jsr DELAY_HALF_SEC
 
+enter_into_loop:
+  jmp loop
 
+print_message:  
+  ;BEGIN Write all the letters
+  ldy #$00 ;start on FF so when i add one it will be 0
 
-
+print_message_eeprom:  
+  lda (charDataVectorLow),y ;load letter from eeprom position indirect in the memory position charDataVector and indexed by Y
+  beq print_message_end ; jump to loop if I load a 0 on lda a zero means the end  of a n .asciiz string
+  jsr print_char 
+  iny
+  jmp print_message_eeprom
+print_message_end:
+  rts 
+  ;END Write all the letters
 
 loop:
   jmp loop
@@ -189,13 +197,24 @@ lcd_send_data:
 
 
 ; Set up the delay
-COUNT_A = $20        ; Outer loop counter
-COUNT_B = $FF        ; Inner loop counter
-
 DELAY_SEC:
-    LDX #COUNT_A     ; Load outer loop count
+  lda #$FF
+  sta delay_COUNT_A
+  lda #$FF
+  sta delay_COUNT_B
+  jmp DELAY_MAIN
+
+DELAY_HALF_SEC:
+  lda #$70
+  sta delay_COUNT_A
+  lda #$FF
+  sta delay_COUNT_B
+  jmp DELAY_MAIN
+
+DELAY_MAIN:
+    LDX delay_COUNT_A     ; Load outer loop count
 OUTER_LOOP:
-    LDY #COUNT_B     ; Load inner loop count
+    LDY delay_COUNT_B     ; Load inner loop count
 INNER_LOOP:
     NOP               ; No operation (takes 2 cycles)
     NOP               ; No operation (takes 2 cycles)
@@ -208,11 +227,113 @@ INNER_LOOP:
     BNE OUTER_LOOP    ; Branch if not zero
     RTS               ; Return from subroutine
 
+
+
 ; Main program
 START:
     JSR DELAY_SEC    ; Call the delay subroutine
+
+
+add_custom_chars:
+  ;BEGIN Add custom char instruction
+  ;8026 lda
+char_0: 
+  lda #($40+$00);+#position_custom_char ;the instruction itself is 0001, write a custom character cgram
+  ;or set cg ram address charter positions are 00,08,10,18,20,28,30,38
+  ;8028 jumps to send intrsuction
+  jsr lcd_send_instruction
+  lda #<fantasma
+  sta charLoadLow
+  lda #>fantasma
+  sta charLoadHigh
+  jsr char_load
+add_custom_chars_end:  
+  rts
+
+add_custom_chars_2:
+  ;BEGIN Add custom char instruction
+  ;8026 lda
+char_0_2: 
+  lda #($40+$00);+#position_custom_char ;the instruction itself is 0001, write a custom character cgram
+  ;or set cg ram address charter positions are 00,08,10,18,20,28,30,38
+  ;8028 jumps to send intrsuction
+  jsr lcd_send_instruction
+  lda #<pacman_boca_abierta
+  sta charLoadLow
+  lda #>pacman_boca_abierta
+  sta charLoadHigh
+  jsr char_load
+add_custom_chars_end_2:  
+  rts
+
+char_load:  
+  ldy #$FF
+char_load_loop:
+  iny  
+  lda (charLoadLow),y
+  jsr lcd_send_data
+  cpy #07
+  bne char_load_loop
+  rts
+  ;END send data for custom character 5x8 char
+
+ .org $d000
+title_1:
+  .asciiz "PACMAN" ;adds a 0 after the last byte
+screen_1_Line1:
+  .byte $02,$A1,$A1,$A1,$A1,%11011011,$A1,$01,$00;pacman boca abierta,punto, punto,fantasma,
+screen_1_Line2:
+  .byte $20,$03,$00;pacman boca abierta,punto, punto,fantasma,
+screen_1_Line3:
+  .byte $20,$02,$00;pacman boca abierta,punto, punto,fantasma
+screen_1_Line4:
+  .byte $20,$20,$03,$00;pacman boca abierta,punto, punto,fantasma
+screen_1_Line5:
+  .byte $20,$20,$02,$00;pacman boca abierta,punto, punto,fantasma
+screen_1_Line6:
+  .byte $20,$20,$20,$03,$00
+screen_1_Line7:
+  .byte $20,$20,$20,$02,$00  
+screen_1_Line8:
+  .byte $20,$20,$20,$20,$03,$00 
+screen_1_Line9:
+  .byte $20,$20,$20,$20,$02,$00 
+screen_1_Line10:
+  .byte $20,$20,$20,$20,$20,$03,$00 
+screen_1_Line11:
+  .byte $20,$20,$20,$20,$20,$02,$A1,$04,$00
+screen_1_Line12:
+  .byte $20,$20,$20,$20,$20,$03,$A1,$04,$00 ;open and close extra for effect
+screen_1_Line13:
+  .byte $20,$20,$20,$20,$20,$02,$A1,$04,$00 ;open and close extra for effect
+screen_1_Line14:
+  .byte $20,$20,$20,$20,$20,$20,$03,$04,$00
+screen_1_Line15:
+  .byte $20,$20,$20,$20,$20,$20,$02,$04,$00
+screen_1_Line16:
+  .byte $20,$20,$20,$20,$20,$20,$20,$03,$00
+screen_1_Line17:
+  .byte $20,$20,$20,$20,$20,$20,$20,$02,$00
+
+fantasma:
+  .byte $00,$0E,$15,$1f,$1f,$1f,$15,$00
+pacman_boca_abierta:
+  .byte $00,$0e,$1f,$1e,$1c,$1e,$1f,$0e
+pacman_boca_cerrada:  
+  .byte $00,$0e,$1f,$1f,$1f,$1f,$1f,$0e
+fantasma_comido:
+  .byte $00,$0E,$15,$1F,$11,$1B,$15,$00
+
+
 
 ;complete the file
   .org $fffc ;go to memory address $fffc of the reset vector
   .word RESET ;store in $FFFC & $FFFD the memory address of the RESET: label  00 80 ($8000 in little endian)
   .word $0000 ;finish completing the values of the eeprom $fffe $ffff with values 00 00
+
+; Positions of LCD characters
+; 	01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
+; 1	80	81	82	83	84	85	86	87	88	89	8A	8B	8C	8D	8F	8E	90	91	92	93
+; 2	C0	C1	C2	C3	C4	C5	C6	C7	C8	C9	CA	CB	CC	CD	CE	CF	D0	D1	D2	D3
+; 3	94	95	96	97	98	99	A0	A1	A2	A3	A4	A5	A6	A7	A8	A9	AA	AB	AC	AD
+; 4	D4	D5	D6	D7	D8	D9	DA	DB	DC	DD	DE	DF	E0	E1	E2	E3	E4	E5	E6	E7
