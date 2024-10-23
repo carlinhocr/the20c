08ee76c64cc142318452750e8fd5d500a58b34d9
carlinhocr <carlos.rienzi@gmail.com>
2024-10-23 14:41:08 -0300

    024 Cursor 5 Buttons

--- code/024_Cursor_5_buttons.asm
+++ code/024_Cursor_5_buttons.asm
@@ -109,7 +109,7 @@ RESET:
 start:
   jsr clear_display
   jsr initiliaze_vectors
-  lda #$3
+  lda #$1
   sta line_cursor
   lda #center_cursor
   sta cursor_position
@@ -587,7 +587,16 @@ button_press_irq:
   .ascii "IRQ Interrupt"
 
 
-; Positions of LCD characters
+lcd_positions_line0:
+  .byte $80,$81,$82,$83,$84,$85,$86,$87,$88,$89,$8A,$8B,$8C,$8D,$8E,$8F,$90,$91,$92,$93
+lcd_positions_line1:
+  .byte $C0,$C1,$C2,$C3,$C4,$C5,$C6,$C7,$C8,$C9,$CA,$CB,$CC,$CD,$CE,$CF,$D0,$D1,$D2,$D3
+lcd_positions_line2:
+  .byte $94,$95,$96,$97,$98,$99,$9A,$9B,$9C,$9D,$9E,$9F,$A0,$A1,$A2,$A3,$A4,$A5,$A6,$A7
+lcd_positions_line3:
+  .byte $D4,$D5,$D6,$D7,$D8,$D9,$DA,$DB,$DC,$DD,$DE,$DF,$E0,$E1,$E2,$E3,$E4,$E5,$E6,$E7
+
+
 ; 	01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
 ; 0	80	81	82	83	84	85	86	87	88	89	8A	8B	8C	8D	8F	8E	90	91	92	93
 ; 1	C0	C1	C2	C3	C4	C5	C6	C7	C8	C9	CA	CB	CC	CD	CE	CF	D0	D1	D2	D3
