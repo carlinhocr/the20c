8d02b508942d2f699e167b06152fd69ec54bdfa7
carlinhocr <carlos.rienzi@gmail.com>
2024-10-31 15:14:16 -0300

    025 GameFlow

--- code/025_GameFlow.asm
+++ code/025_GameFlow.asm
@@ -146,6 +146,15 @@ RESET:
 ; 3	D4	D5	D6	D7	D8	D9	DA	DB	DC	DD	DE	DF	E0	E1	E2	E3	E4	E5	E6	E7
 
 
+; Posible ordinal positions
+
+; 01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
+; 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  A,  B,  C,  D,  E,  F,  10, 11, 12, 13
+; 14, 15, 16, 17, 18, 19, 1A, 1B, 1C, 1D, 1E, 1F, 20, 21, 22, 23, 24, 25, 26, 27
+; 28, 29, 2A, 2B, 2C, 2D, 2E, 2F, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 3A, 3B
+; 3C, 3D, 3E, 3F, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 4A, 4B, 4C, 4D, 4E, 4F
+
+
 ; PLAN
 ; create a matrix of cursor positions in memory 4x20
 ;
@@ -153,6 +162,13 @@ RESET:
 ;
 ; create a function that reads the video ram and position by position puts it in the
 ; correct lcd position drawing the screen
+;
+; an array of aliens with the realtive position of each
+;
+;a function that reads that array and writes to the screen 
+;
+;
+
 
 ; start:
 ;   jsr clear_display
