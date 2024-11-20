c0be5cb631da95c7e6474d7b07b514f7d7c9b51c
carlinhocr <carlos.rienzi@gmail.com>
2024-11-20 17:45:53 -0300

    031 7SegmentDisplayROM

--- code/031_7SegmentDisplayROM.py
+++ code/031_7SegmentDisplayROM.py
@@ -1,4 +1,4 @@
-rom = bytearray([0xFF] *16384) # create an array of 32768 ea
+rom = bytearray([0xFF] *2048) # create an array of 32768 ea
 
 rom[0] = 0b01110111 #number 0 in the 7 digit display
 rom[1] = 0b00010001 #number 1 in the 7 digit display
