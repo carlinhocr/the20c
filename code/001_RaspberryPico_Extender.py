ceb9152cc5bb07eb716064d4cc8fd7b94f282467
carlinhocr <carlos.rienzi@gmail.com>
2024-12-12 15:12:27 -0300

    001 Raspberry Pico Protocol analyzer

--- code/001_RaspberryPico_Extender.py
+++ code/001_RaspberryPico_Extender.py
@@ -1,10 +1,23 @@
 import machine
 import utime
 import urandom
+import mcp23017
+
+#configure i2c pins
+sdaPIN=machine.Pin(20)
+sdlPIN=machine.Pin(21)
+#configure i2c settings
+i2c=machine.I2C(0,sda=sdaPIN, scl=sclPIN, freq=400000)
+#create an object for using the chip
+mcp = mcp23017.MCP23017(i2c,0x20)
+
 
 address_number = [3,2,1,0]
 address_pin = []
 clock = 28
+
+for mcp_pin_number in range(0,15):
+    mcp[mcp_pin_number].input()
 
 for pin_number in address_number:
     print(pin_number)
@@ -16,6 +29,12 @@ address = ""
 for pin in address_pin:
     print(pin.value())
     address = address + str(pin.value())
+mcp_data = ""
+print("mcp")
+for mcp_pin_number in range(8,15):
+    print(mcp[mcp_pin_number].value())
+    mcp_data = mcp_data + int(mcp[mcp_pin_number].value())
+    
 print (address)
 print (hex(int(address,2)))
 print ("{0:0>4X}".format(int(address, 2))) #use for address fields
