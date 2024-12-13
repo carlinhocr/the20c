f977dc2e845271a9c5c55025d7b0542a4f77dec7
carlinhocr <carlos.rienzi@gmail.com>
2024-12-13 15:44:51 -0300

    001 Raspberry Pico Protocol analyzer

--- code/001_RaspberryPico_Extender.py
+++ code/001_RaspberryPico_Extender.py
@@ -12,6 +12,7 @@ sclPIN=machine.Pin(27)
 i2c=machine.I2C(1,sda=sdaPIN, scl=sclPIN, freq=400000)
 #create an object for using the chip
 mcp = mcp23017.MCP23017(i2c,0x20)
+mcp1 = mcp23017.MCP23017(i2c,0x21)
 utime.sleep_ms(1)
 
 address_number = [3,2,1,0]
@@ -23,7 +24,11 @@ clock = 28
 for mcp_pin_number in range(0,15):
     print(mcp_pin_number)
     #mcp[mcp_pin_number].input()
-    mcp[1].input(pull=1)
+    mcp[mcp_pin_number].input(pull=1)
+
+mcp1.porta.mode = 0xff #direction 0 output 1 Input
+mcp1.portb.mode = 0xff #direction 0 output 1 Input
+
 
 for pin_number in address_number:
     print(pin_number)
@@ -61,3 +66,17 @@ mcp_data_hexa="{0:0>4X}".format(int(mcp_data, 2))
 print(mcp_data+" "+mcp_data_hexa)
 print(hex(mcp.porta.gpio))
 print(str("{0:0>4X}".format(mcp.porta.gpio, 2)))
+
+print("MCP 1 PORT A")
+print(hex(mcp1.porta.gpio))
+mcp1_hexa = str("{0:0>4X}".format(mcp1.porta.gpio, 2))
+print(mcp1_hexa)
+print(bin(mcp1.porta.gpio))
+mcp1_bin=str("{0:0>4b}".format(mcp1.porta.gpio, 2))
+print(mcp1_bin)
+print(mcp1_bin + " " +mcp1_hexa)
+print("MCP 1 PORT B & A")
+print(str("{0:0>4X}".format(mcp1.gpio, 4)))
+mcp1_full_hexa = str("{0:0>4X}".format(mcp1.gpio, 4))
+mcp1_full_bin = str("{0:0>4b}".format(mcp1.gpio, 4))
+print (mcp1_full_bin + "   " + mcp1_full_hexa)
