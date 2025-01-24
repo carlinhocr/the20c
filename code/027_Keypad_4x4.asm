586d4be61850e52732b8185ad8c3fcd9fe7488f9
carlinhocr <carlos.rienzi@gmail.com>
2025-01-24 15:06:22 -0300

    027 Keypad 4x4 keyboardbuffer

--- code/027_Keypad_4x4.asm
+++ code/027_Keypad_4x4.asm
@@ -185,22 +185,6 @@ RESET:
 ;-----------------------------------------------------------------------------------
 ;-----------------------------------------------------------------------------------
 
-; Positions of LCD characters
-; 	01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
-; 0	80	81	82	83	84	85	86	87	88	89	8A	8B	8C	8D	8F	8E	90	91	92	93
-; 1	C0	C1	C2	C3	C4	C5	C6	C7	C8	C9	CA	CB	CC	CD	CE	CF	D0	D1	D2	D3
-; 2	94	95	96	97	98	99	9a	9b	9c	9d	9e	9f	a0	a1	a2	a3	a4	a5	a6	a7
-; 3	D4	D5	D6	D7	D8	D9	DA	DB	DC	DD	DE	DF	E0	E1	E2	E3	E4	E5	E6	E7
-
-
-; Posible ordinal positions
-
-; 01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
-; 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  A,  B,  C,  D,  E,  F,  10, 11, 12, 13
-; 14, 15, 16, 17, 18, 19, 1A, 1B, 1C, 1D, 1E, 1F, 20, 21, 22, 23, 24, 25, 26, 27
-; 28, 29, 2A, 2B, 2C, 2D, 2E, 2F, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 3A, 3B
-; 3C, 3D, 3E, 3F, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 4A, 4B, 4C, 4D, 4E, 4F
-
 
 programStart:
   jsr screenInit
@@ -210,7 +194,7 @@ programStart:
 
 programLoop:  
   jsr keyboardScan
-  ;jsr printKeyboardBuffer
+  jsr printKeyboardBuffer
   jmp programLoop  
 
 
@@ -386,28 +370,28 @@ addColumnNumber:
   clc
   adc columnNumberDetected
   sta keyPressedPosition
-  jmp writeKeyboardBufferMapKey
-  ;jmp addKeyToKeyboardBufferMapKey
+  ;jmp writeKeyboardBufferMapKey
+  jmp addKeyToKeyboardBufferMapKey
 
-writeKeyboardBufferMapKey:  
-  lda #$d4 ;position cursor at the start of sthe fourth line
-  jsr lcd_send_instruction
-  ldy keyPressedPosition
-  lda (keymapMemoryLowZeroPage),Y
-  jsr print_char 
-  jsr delay_1_sec
-  jsr welcomeMessage
-  rts
-
-; addKeyToKeyboardBufferMapKey:  
+; writeKeyboardBufferMapKey:  
+;   lda #$d4 ;position cursor at the start of sthe fourth line
+;   jsr lcd_send_instruction
 ;   ldy keyPressedPosition
-;   lda (keymapMemoryLowZeroPage),Y ;store character in accumulator
-;   ldy keyboardBufferPointer
-;   sta (keyboardBufferZPLow),Y ;save character to pointer
-;   lda #$1
-;   clc 
-;   adc keyboardBufferPointer
+;   lda (keymapMemoryLowZeroPage),Y
+;   jsr print_char 
+;   jsr delay_1_sec
+;   jsr welcomeMessage
 ;   rts
+
+addKeyToKeyboardBufferMapKey:  
+  ldy keyPressedPosition
+  lda (keymapMemoryLowZeroPage),Y ;store character in accumulator
+  ldy keyboardBufferPointer
+  sta (keyboardBufferZPLow),Y ;save character to pointer
+  lda #$1
+  clc 
+  adc keyboardBufferPointer
+  rts
 
 printKeyboardBuffer:
   ldy #$FF
@@ -456,6 +440,22 @@ loadKeymapEnd:
 ;--------------------------------SCREEN MANAGEMENT----------------------------------
 ;-----------------------------------------------------------------------------------
 ;-----------------------------------------------------------------------------------
+
+; Positions of LCD characters
+; 	01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
+; 0	80	81	82	83	84	85	86	87	88	89	8A	8B	8C	8D	8F	8E	90	91	92	93
+; 1	C0	C1	C2	C3	C4	C5	C6	C7	C8	C9	CA	CB	CC	CD	CE	CF	D0	D1	D2	D3
+; 2	94	95	96	97	98	99	9a	9b	9c	9d	9e	9f	a0	a1	a2	a3	a4	a5	a6	a7
+; 3	D4	D5	D6	D7	D8	D9	DA	DB	DC	DD	DE	DF	E0	E1	E2	E3	E4	E5	E6	E7
+
+
+; Posible ordinal positions
+
+; 01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
+; 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  A,  B,  C,  D,  E,  F,  10, 11, 12, 13
+; 14, 15, 16, 17, 18, 19, 1A, 1B, 1C, 1D, 1E, 1F, 20, 21, 22, 23, 24, 25, 26, 27
+; 28, 29, 2A, 2B, 2C, 2D, 2E, 2F, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 3A, 3B
+; 3C, 3D, 3E, 3F, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 4A, 4B, 4C, 4D, 4E, 4F
 
 screenInit:
   jsr initilize_display
