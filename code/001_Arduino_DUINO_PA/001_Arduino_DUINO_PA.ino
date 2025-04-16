470026337db429b1ff7c685bc4715c07c7c4be9a
carlinhocr <carlos.rienzi@gmail.com>
2025-04-16 14:25:11 -0300

    001 arduino PA completing opcodes

--- code/001_Arduino_DUINO_PA/001_Arduino_DUINO_PA.ino
+++ code/001_Arduino_DUINO_PA/001_Arduino_DUINO_PA.ino
@@ -79,31 +79,18 @@ void onClock (){
     int bit = digitalRead(DATA[n]) ? 1:0; //? ternary operator if TRUE then 1 else 0
     Serial.print(bit);
     data = (data << 1) + bit;
-  }
+  };
+  //unsigned int prefix_address = 0;
+  //for (int n=0;n<4;n++){
+  //  int bit=address[n];
+  //  prefix_address=(prefix_address << 1) +bit;
+  //}
   char *instruction=sync_string(SYNC_VALUE,RESET_VALUE,data);
-  unsigned int prefix_address = 0;
-  for (int n=0;n<4;n++){
-    int bit=address[n];
-    prefix_address=(prefix_address << 1) +bit;
-  }
-  char *chip=find_chip(prefix_address);
-  sprintf(output, " %04x  %04c %01c %02x %01c %04c",address,digitalRead(READ_WRITE)?'r':'W',SYNC_VALUE,data,' ',chip,' ');
+  char *chip=find_chip(address);
+  sprintf(output, " %04x  %04c %01c %02x %01c %04c %04c",address,digitalRead(READ_WRITE)?'r':'W',SYNC_VALUE,data,' ',chip,' ');
   Serial.print(output);
   Serial.println(instruction);
 }
-
-char *find_chip(unsigned int prefix_address) {
-  if (prefix_address >= 8000){
-    return "RAM ";
-  } else if (prefix_address >= 0000 and prefix_address < 4000){
-    return "ROM ";
- } else if (prefix_address >= 6000 and prefix_address < 7000){
-    return "VIA1";
- }  else if (prefix_address >= 7000 and prefix_address < 8000){
-    return "VIA2";
- } else {
-    return "UNKW";
- };
 
 char *sync_string(char SYNC_VALUE,char RESET_VALUE,unsigned int data) {
   if (RESET_VALUE == 'R'){
@@ -307,6 +294,20 @@ char *sync_string(char SYNC_VALUE,char RESET_VALUE,unsigned int data) {
     return  "   " ;
   }
 } 
+
+char *find_chip(unsigned int prefix_address) {
+  if (prefix_address >= 8000){
+    return "RAM ";
+  } else if (prefix_address >= 0000 and prefix_address < 4000){
+    return "ROM ";
+ } else if (prefix_address >= 6000 and prefix_address < 7000){
+    return "VIA1";
+ }  else if (prefix_address >= 7000 and prefix_address < 8000){
+    return "VIA2";
+ } else {
+    return "UNKW";
+ };
+}
 
 void loop() {
 }
