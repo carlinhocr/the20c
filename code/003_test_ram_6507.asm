f36413d6eddaee6c21090704fbb8fa4a7baa7b6d
carlinhocr <carlos.rienzi@gmail.com>
2025-05-18 13:21:07 -0300

    002 003 rom & ram test 6507

---
+++ code/003_test_ram_6507.asm
@@ -0,0 +1,22 @@
+
+
+;test simpre instructions in an epprom
+
+  .org $0800 ;changed for the 6507 to use with A12 to A0 addresses
+
+RESET:
+  ;initialize stack
+  lda #$42 
+  sta $0200
+INCRAM:  
+  inc $0200
+  jmp INCRAM
+  
+
+;complete the file
+  .org $fffa
+  .word RESET ;a word is 16 bits or two bytes in this case $fffa and $fffb
+  .org $fffc ;go to memory address $fffc of the reset vector
+  .word RESET ;store in $FFFC & $FFFD the memory address of the RESET: label  00 80 ($8000 in little endian)
+  .org $fffe
+  .word RESET ;a word is 16 bits or two bytes in this case $fffe and $ffff
