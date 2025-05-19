0bc60b4850198e2accfe6aa3e0d6a75f1b96f5b1
carlinhocr <carlos.rienzi@gmail.com>
2025-05-19 15:12:17 -0300

    002 003 test eeprom & ram 6507

--- code/003_test_ram_6507.asm
+++ code/003_test_ram_6507.asm
@@ -1,9 +1,9 @@
 
 
-;test simpre instructions in an epprom
+; ;test simpre instructions in an epprom
 
-  .org $1000 ;changed for the 6507 to use with A12 to A0 addresses A12=1
-             ;the rest is zero
+   .org $1000 ;changed for the 6507 to use with A12 to A0 addresses A12=1
+;              ;the rest is zero
 
 RESET:
   ;initialize stack
@@ -23,27 +23,23 @@ INCRAM:
   .word RESET ;a word is 16 bits or two bytes in this case $fffe and $ffff
 ;test simpre instructions in an epprom
 
-
- .org $2000 ;changed for the 6507 to use with A12 to A0 addresses A12=1
-             ;the rest is zero
-
-RESET2:
-  ;initialize stack
-  lda #$42 
-  sta $0200
-INCRAM2:  
-  inc $0200
-  jmp INCRAM2
+; RESET:
+;   ;initialize stack
+;   lda #$42 
+;   sta $0200
+; INCRAM:  
+;   inc $0200
+;   jmp INCRAM2
     
 
 
 ;complete the file
  .org $2ffa
- .word RESET2 ;a word is 16 bits or two bytes in this case $fffa and $fffb
+ .word RESET ;a word is 16 bits or two bytes in this case $fffa and $fffb
  .org $2ffc ;go to memory address $fffc of the reset vector
- .word RESET2 ;store in $FFFC & $FFFD the memory address of the RESET: label  00 80 ($8000 in little endian)
+ .word RESET ;store in $FFFC & $FFFD the memory address of the RESET: label  00 80 ($8000 in little endian)
  .org $2ffe
- .word RESET2 ;a word is 16 bits or two bytes in this case $fffe and $ffff
+ .word RESET;a word is 16 bits or two bytes in this case $fffe and $ffff
 
   .org $3000 ;changed for the 6507 to use with A12 to A0 addresses A12=1
              ;the rest is zero
