7a9019127304f6f3a2cb96ee33f760df7bdbbe28
carlinhocr <carlos.rienzi@gmail.com>
2025-05-22 14:20:48 -0300

    002 003 test eeprom & ram 6507

--- code/003_test_ram_6507.asm
+++ code/003_test_ram_6507.asm
@@ -2,7 +2,7 @@
 
 ; ;test simpre instructions in an epprom
 
-   .org $1000 ;changed for the 6507 to use with A12 to A0 addresses A12=1
+   .org $8000 ;changed for the 6507 to use with A12 to A0 addresses A12=1
 ;              ;the rest is zero
 
 RESET:
@@ -15,10 +15,10 @@ INCRAM:
     
 
 ;complete the file
-  .org $1ffa
+  .org $8ffa
   .word RESET ;a word is 16 bits or two bytes in this case $fffa and $fffb
-  .org $1ffc ;go to memory address $fffc of the reset vector
+  .org $8ffc ;go to memory address $fffc of the reset vector
   .word RESET ;store in $FFFC & $FFFD the memory address of the RESET: label  00 80 ($8000 in little endian)
-  .org $1ffe
+  .org $8ffe
   .word RESET ;a word is 16 bits or two bytes in this case $fffe and $ffff
 
