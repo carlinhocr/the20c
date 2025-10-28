1d264bf26f474c521e3afbd54ddf472eae032b26
carlinhocr <carlos.rienzi@gmail.com>
2025-10-28 16:12:58 -0300

    Sound SID playSidNotes

--- code/040_Sound_SID_and_SquareWave.asm
+++ code/040_Sound_SID_and_SquareWave.asm
@@ -101,7 +101,7 @@ lcdROMPositionsLowZeroPage =$38
 lcdROMPositionsHighZeroPage =$39
 initialScreenZeroPageLow=$3a
 initialScreenZeroPageHigh=$3b
-record_lenght=$3c ;it is a memory position
+;record_lenght=$3c ;it is a memory position
 serialDataVectorLow = $3d
 serialDataVectorHigh = $3e
 serialCharperLines = $3f
@@ -120,6 +120,30 @@ sidLocationLow=$58
 sidLocationHigh=$59
 sidNotesLow=$5a
 sidNotesHigh=$5b
+
+;constants
+fill=$43 ;letter C
+totalScreenLenght4Lines=$50
+totalScreenLenght=$3c ;make it only 3 lines long 3c = 60 decimal
+totalLineLenght=$13 ;20 positions in hexadecimal is 13
+end_char=$ff
+cblank=$20
+
+pos_line1=$8B
+pos_line2=$CB
+pos_line3=$9F
+pos_line4=$DF
+
+record_lenght=$09
+
+lenght_screen_lines=$04; 0 to 3
+lenght_ascii_line_characters=$20
+lenght_screen_characters=$50 ;80 in decimal
+pos_lcd_initial_line0=$80
+pos_lcd_initial_line1=$C0
+pos_lcd_initial_line2=$94
+pos_lcd_initial_line3=$D4
+
 
 ;Memory Mappings
 ;these are constants where we reflect the number of the memory position
@@ -179,12 +203,28 @@ RESET:
 programStart:
   ;initialize variables, vectors, memory mappings and constans
   ;configure stack and enable interrupts
-  ;jsr viaLcdInit
+  jsr viaLcdInit
+  jsr screenInit
+  jsr sidPlayerMessage  
   ;jsr squareTest
   ;jsr sidTest
   jsr sidNotesExamplePlay
 loop:
   jmp loop
+
+sidPlayerMessage:
+  ;Draw Screen 1 Final Demo
+  lda #<screen1_sidPlayer
+  sta charDataVectorLow
+  lda #>screen1_sidPlayer
+  sta charDataVectorHigh
+  jsr print_ascii_screen  
+
+screen1_sidPlayer:
+  .asciiz "                    "
+  .asciiz "       SID          "
+  .asciiz "      PLAYER        "
+  .asciiz "                    "    
 
 squareTest:
   jsr viaSoundInit ;j
@@ -240,6 +280,65 @@ viaLcdInit:
 ;-----------------------------------------------------------------------------------
 ;--------------------------------VIALCDINIT-----------------------------------------
 ;-----------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+
+;BEGIN------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+;--------------------------------ASCII----------------------------------------------
+;-----------------------------------------------------------------------------------
+
+set_position_lcd_line0:
+  lda #pos_lcd_initial_line0
+  jsr lcd_send_instruction 
+  jmp print_ascii_screen_eeprom
+set_position_lcd_line1:
+  lda #pos_lcd_initial_line1
+  jsr lcd_send_instruction 
+  jmp print_ascii_screen_eeprom
+set_position_lcd_line2:
+  lda #pos_lcd_initial_line2
+  jsr lcd_send_instruction 
+  jmp print_ascii_screen_eeprom
+set_position_lcd_line3:
+  lda #pos_lcd_initial_line3
+  jsr lcd_send_instruction 
+  jmp print_ascii_screen_eeprom
+reset_screen_position:
+  lda #pos_lcd_initial_line0
+  jsr lcd_send_instruction 
+  jmp print_ascii_screen_end
+
+print_ascii_screen:  
+  ;BEGIN print_ascii_screen
+  jsr clear_display
+  ldx #$ff ; start as ff so when i add 1 it goes to zero
+  ldy #$ff ; jsut at the begginning so it would got all the 80 characters of the screen
+print_ascii_screen_line:  
+  inx
+  cpx #$00
+  beq set_position_lcd_line0
+  cpx #$01
+  beq set_position_lcd_line1
+  cpx #$02
+  beq set_position_lcd_line2
+  cpx #$03
+  beq set_position_lcd_line3
+  cpx #$04
+  beq reset_screen_position ; to reset the screen to initial position
+print_ascii_screen_eeprom:
+  iny ;so it would go out of a last byte equal 0 loop
+  lda (charDataVectorLow),y ;load letter from eeprom position indirect in the memory position charDataVector and indexed by Y
+  beq print_ascii_screen_line ; jump to loop if I load a 0 on lda a zero means the end  of a n .asciiz string
+  jsr print_char 
+  jmp print_ascii_screen_eeprom
+print_ascii_screen_end:
+  rts 
+  ;END print_ascii_screen
+
+
+;END---------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+;--------------------------------ASCII----------------------------------------------
 ;-----------------------------------------------------------------------------------
 
 
@@ -1612,6 +1711,400 @@ notesInHexaSquareWave:
 ;------------------------------SOUND SQUARE PROGRAM---------------------------------
 ;-----------------------------------------------------------------------------------
 ;-----------------------------------------------------------------------------------
+
+
+;BEGIN------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+;--------------------------------SCREEN MANAGEMENT----------------------------------
+;-----------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+
+; Positions of LCD characters
+; 	01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
+; 0	80	81	82	83	84	85	86	87	88	89	8A	8B	8C	8D	8F	8E	90	91	92	93
+; 1	C0	C1	C2	C3	C4	C5	C6	C7	C8	C9	CA	CB	CC	CD	CE	CF	D0	D1	D2	D3
+; 2	94	95	96	97	98	99	9a	9b	9c	9d	9e	9f	a0	a1	a2	a3	a4	a5	a6	a7
+; 3	D4	D5	D6	D7	D8	D9	DA	DB	DC	DD	DE	DF	E0	E1	E2	E3	E4	E5	E6	E7
+
+
+; Posible ordinal positions
+
+; 01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
+; 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  A,  B,  C,  D,  E,  F,  10, 11, 12, 13
+; 14, 15, 16, 17, 18, 19, 1A, 1B, 1C, 1D, 1E, 1F, 20, 21, 22, 23, 24, 25, 26, 27
+; 28, 29, 2A, 2B, 2C, 2D, 2E, 2F, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 3A, 3B
+; 3C, 3D, 3E, 3F, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 4A, 4B, 4C, 4D, 4E, 4F
+
+screenInit:
+  jsr initilize_display
+  jsr clear_display
+  jsr loadCursorPositions
+  jsr loadScreen
+  jsr drawScreen
+  jsr DELAY_SEC   
+  jsr clearScreenBuffer
+  jsr drawScreen 
+  rts
+
+; create a matrix of cursor positions in memory 4x20
+loadCursorPositions:
+  ;load vectors
+  lda #lcdCharPositionsLow
+  sta lcdCharPositionsLowZeroPage ; to use indirect addressing with y
+  lda #lcdCharPositionsHigh
+  sta lcdCharPositionsHighZeroPage
+  lda #<lcd_positions
+  sta lcdROMPositionsLowZeroPage
+  lda #>lcd_positions
+  sta lcdROMPositionsHighZeroPage
+  ldy #$ff
+loadCursorPositionsLoop:
+  iny
+  cpy #totalScreenLenght4Lines 
+  ;cpy #totalScreenLenght ;80 decimal it counts from 0 to 49 and then at 50 is the 81 number quit
+  beq loadCursorPositionsEnd
+  ; copy from ROM to RAM the LCD positions
+  lda (lcdROMPositionsLowZeroPage),Y
+  sta (lcdCharPositionsLowZeroPage),Y
+  jmp loadCursorPositionsLoop
+loadCursorPositionsEnd:
+  rts
+
+loadScreen:
+  ;load vectors
+  lda #screenBufferLow
+  sta screenMemoryLow ; to use indirect addressing with y
+  lda #screenBufferHigh
+  sta screenMemoryHigh
+  lda #<initialScreen
+  sta initialScreenZeroPageLow
+  lda #>initialScreen
+  sta initialScreenZeroPageHigh
+  ldy #$ff
+loadScreenLoop:
+  iny
+  cpy #totalScreenLenght ;80 decimal it counts from 0 to 49 and then at 50 is the 81 number quit
+  beq loadCursorPositionsEnd
+  ; copy from ROM to RAM the LCD positions
+  lda (initialScreenZeroPageLow),Y
+  sta (screenMemoryLow),Y
+  jmp loadScreenLoop
+loadScreenEnd:
+  rts
+
+drawScreen:
+  ldy #$ff
+drawScreenLoop:
+  iny
+  cpy #totalScreenLenght ;80 decimal it counts from 0 to 49 and then at 50 is the 81 number quit
+  beq drawScreenEnd
+  ;position cursor
+  lda (lcdCharPositionsLowZeroPage),Y ;load cursor position
+  jsr lcd_send_instruction ; position cursor
+  ;write screen character
+  lda (screenMemoryLow),Y ;load character
+  jsr print_char 
+  jmp drawScreenLoop
+drawScreenEnd:
+  rts
+
+
+print_screen:  
+  ;BEGIN Write all the letters 
+  ldy #$00 ;first byte is the position of the line
+print_screen_load_position:
+  lda (charDataVectorLow),y
+  cmp #end_char;compare to ending character
+  beq print_screen_end ;jump to loop if I load a 0 on lda a zero means the end  of a n .asciiz string
+  jsr lcd_send_instruction 
+  ldx #$00
+print_screen_eeprom:  
+  iny
+  inx
+  cpx record_lenght ; record lenght is a memory position now
+  beq print_screen_load_position
+  lda (charDataVectorLow),y ;load letter from eeprom position indirect in the memory position charDataVector and indexed by Y
+  jsr print_char 
+  jmp print_screen_eeprom
+print_screen_end:
+  rts   
+
+print_message:  
+  ;BEGIN Write all the letters
+  ldy #$00 ;start on FF so when i add one it will be 0
+
+print_message_eeprom:  
+  lda (charDataVectorLow),y ;load letter from eeprom position indirect in the memory position charDataVector and indexed by Y
+  beq print_message_end ; jump to loop if I load a 0 on lda a zero means the end  of a n .asciiz string
+  jsr print_char 
+  iny
+  jmp print_message_eeprom
+print_message_end:
+  rts 
+  ;END Write all the letters  
+
+clearScreenBuffer: 
+  ldy #$FF 
+clearScreenBufferLoop:
+  iny      
+  cpy #$50
+  beq clearScreenBufferEnd
+  lda #cblank ;load ship form
+  sta (screenMemoryLow),y ;at the alien position en Y draw the alien ship on the accumulator
+  jmp clearScreenBufferLoop 
+clearScreenBufferEnd: 
+  rts    
+
+
+;END--------------------------------------------------------------------------------  
+;-----------------------------------------------------------------------------------
+;--------------------------------SCREEN MANAGEMENT----------------------------------
+;-----------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+
+
+
+;BEGIN------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+;--------------------------------LCD COMMANDS---------------------------------------
+;-----------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------  
+
+clear_display:
+; BEGIN clear display instruction  on port B
+  lda #%00000001 ;the instruction itself is 00000001
+  jsr lcd_send_instruction
+  ; END clear display instruction on port B 
+
+
+initilize_display:
+; BEGIN clear display instruction  on port B
+  lda #%00000001 ;the instruction itself is 00000001
+  jsr lcd_send_instruction
+  ; END clear display instruction on port B  
+
+  ; BEGIN send the instruction function set on port B
+  lda #%00111000 ;the instruction itself is 001, data lenght 8bits(1), Number Display lines 2 (1)
+            ;and Character Font 5x8 (0), last two bits are unused
+  jsr lcd_send_instruction 
+  ; END send the instruction function set on port B
+
+  ;BEGIN Turn on Display instruction
+  lda #%00001100 ;the instruction itself is 0001, Display On(1), Cursor Off (0)
+            ;and Cursor Blinking Off (0)
+  jsr lcd_send_instruction 
+  ; END Turn on Display instruction
+
+   ;BEGIN Entry Mode Set instruction
+  lda #%00000110 ;the instruction itself is 00001, Put next character to the right (1)
+            ;and Scroll Display Off (0)
+  jsr lcd_send_instruction
+  ; END Entry Mode Set instruction
+  rts
+
+lcd_wait:
+  pha ; push to preserve the contents of the acummulator register
+  ;set LCD_PORTB to all inputs so we can read the busy flag
+  lda #$00000000 ;port b ins input
+  sta LCD_DDRB 
+lcd_busy:
+  ;set register select to 0 and RW to 1 to read the busy flag
+  lda #RW ;set RW RW = %01000000 ; Read/Write Signal
+  sta LCD_PORTA
+  lda #(RW | E) ;do the enable and do not era the RW bit
+  sta LCD_PORTA
+  ;this will give us the info from the busy flags and the counter 01 BF AC AC AC AC AC AC AC
+  ;on port B so we read it
+  lda LCD_PORTB
+  and #%10000000 ;and the accumulator to loose all bits but the 7 bit (from 7 to 0)
+                ; on the acummulator I will now have only the Busy Flag result
+  bne lcd_busy ; branch if the zero flag is not set
+  ;turn off the enable bit
+  lda #RW ;set RW RW = %01000000 ; Read/Write Signal
+  sta LCD_PORTA
+  ;set all port B pins as output
+  lda #%11111111  ;load all ones equivalent to $FF to make it output
+  sta LCD_DDRB ;store the accumulator in the data direction register for Port B
+  pla ; pull to restablish the contents of the acummulator register
+  rts
+
+
+lcd_send_instruction:
+  pha ;push the accumulator value to the stack so we can have it back a the end of the subroutine
+  jsr lcd_wait
+  sta LCD_PORTB
+            
+  lda #%0  ;Clear RS,RW and E bit on Port A  
+  sta LCD_PORTA ;     
+
+  ;togle the enable bit in order to send the instruction
+  ;RS is zero so we are sending instructions
+  ;RW is zero so we are writing
+  lda #E ;enable bit is 1 , so we turn on the chip and execute the instruction.
+  sta LCD_PORTA ; 
+
+  lda #%0  ;Clear RS,RW and E bit on Port A  
+  sta LCD_PORTA ;  
+  pla ;pull the accumulator value to the stack so we can have it back a the end of the subroutine
+  rts ; return from the subroutine
+
+print_char:
+  pha ;push the accumulator value to the stack so we can have it back a the end of the subroutine
+  jsr lcd_wait
+  sta LCD_PORTB
+
+  ;RS is one so we are sending data
+  ;RW is zero so we are writing
+  lda #RS  ;Set RS, and clear RW and E bit on Port A  
+  sta LCD_PORTA ;     
+
+  ;togle the enable bit in order to send the instruction
+  lda #(RS | E );RS and enable bit are 1 , we OR them and send the data
+  sta LCD_PORTA ; 
+
+  lda #RS  ;Set RS, and clear RW and E bit on Port A  
+  sta LCD_PORTA ; 
+  pla ;pull the accumulator value to the stack so we can have it back a the end of the subroutine
+  rts
+
+lcd_positions:
+lcd_positions_line0:
+  .byte $80,$81,$82,$83,$84,$85,$86,$87,$88,$89,$8A,$8B,$8C,$8D,$8E,$8F,$90,$91,$92,$93
+lcd_positions_line1:
+  .byte $C0,$C1,$C2,$C3,$C4,$C5,$C6,$C7,$C8,$C9,$CA,$CB,$CC,$CD,$CE,$CF,$D0,$D1,$D2,$D3
+lcd_positions_line2:
+  .byte $94,$95,$96,$97,$98,$99,$9A,$9B,$9C,$9D,$9E,$9F,$A0,$A1,$A2,$A3,$A4,$A5,$A6,$A7
+lcd_positions_line3:
+  .byte $D4,$D5,$D6,$D7,$D8,$D9,$DA,$DB,$DC,$DD,$DE,$DF,$E0,$E1,$E2,$E3,$E4,$E5,$E6,$E7
+
+
+; 	01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20
+; 0	80	81	82	83	84	85	86	87	88	89	8A	8B	8C	8D	8F	8E	90	91	92	93
+; 1	C0	C1	C2	C3	C4	C5	C6	C7	C8	C9	CA	CB	CC	CD	CE	CF	D0	D1	D2	D3
+; 2	94	95	96	97	98	99	9a	9b	9c	9d	9e	9f	a0	a1	a2	a3	a4	a5	a6	a7
+; 3	D4	D5	D6	D7	D8	D9	DA	DB	DC	DD	DE	DF	E0	E1	E2	E3	E4	E5	E6	E7
+initialScreen:
+  .byte fill,fill,fill,fill,fill,fill,fill,fill,fill,fill
+  .byte fill,fill,fill,fill,fill,fill,fill,fill,fill,fill
+  .byte fill,fill,fill,fill,fill,fill,fill,fill,fill,fill
+  .byte fill,fill,fill,fill,fill,fill,fill,fill,fill,fill
+  .byte fill,fill,fill,fill,fill,fill,fill,fill,fill,fill
+  .byte fill,fill,fill,fill,fill,fill,fill,fill,fill,fill
+  .byte fill,fill,fill,fill,fill,fill,fill,fill,fill,fill
+  .byte fill,fill,fill,fill,fill,fill,fill,fill,fill,fill
+
+; left_cursor_endings:
+;  ; .byte  $80,$c0,$94,$d4
+;   .byte  $88,$c8,$9c,$dc  
+
+; right_cursor_endings:
+;   .byte  $93,$d3,$a7,$e7
+
+; ; up_cursor_endings:
+; ;   .byte $80,$81,$82,$83,$84,$85,$86,$87,$88,$89,$8A,$8B,$8C,$8D,$8F,$8E,$90,$91,$92,$93
+; up_cursor_endings: ;so it is all in one line
+;   .byte $D4,$D5,$D6,$D7,$D8,$D9,$DA,$DB,$DC,$DD,$DE,$DF,$E0,$E1,$E2,$E3,$E4,$E5,$E6,$E7
+  
+; down_cursor_endings:
+;   .byte $D4,$D5,$D6,$D7,$D8,$D9,$DA,$DB,$DC,$DD,$DE,$DF,$E0,$E1,$E2,$E3,$E4,$E5,$E6,$E7
+
+
+
+;END--------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+;--------------------------------LCD COMMANDS---------------------------------------
+;-----------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+
+
+;BEGIN------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+;--------------------------------TIME MANAGEMENT------------------------------------
+;-----------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+
+delay_1_sec:
+  jsr DELAY_SEC
+  rts
+
+delay_2_sec:
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  rts
+
+delay_3_sec:
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  rts
+
+delay_4_sec:
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  rts 
+
+delay_5_sec:
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  jsr DELAY_SEC
+  rts
+
+DELAY_onetenth_SEC:
+  lda #$10
+  sta delay_COUNT_A
+  lda #$FF
+  sta delay_COUNT_B
+  jmp DELAY_MAIN
+
+DELAY_two_tenth_SEC:
+  lda #$10
+  sta delay_COUNT_A
+  lda #$FF
+  sta delay_COUNT_B
+  jmp DELAY_MAIN   
+
+DELAY_SEC:
+  lda #$FF
+  sta delay_COUNT_A
+  lda #$FF
+  sta delay_COUNT_B
+  jmp DELAY_MAIN
+
+DELAY_HALF_SEC:
+  lda #$50
+  sta delay_COUNT_A
+  lda #$FF
+  sta delay_COUNT_B
+  jmp DELAY_MAIN
+
+DELAY_MAIN:
+    LDX delay_COUNT_A     ; Load outer loop count
+OUTER_LOOP:
+    LDY delay_COUNT_B     ; Load inner loop count
+INNER_LOOP:
+    NOP               ; No operation (takes 2 cycles)
+    NOP               ; No operation (takes 2 cycles)
+    NOP               ; No operation (takes 2 cycles)
+    NOP               ; No operation (takes 2 cycles)
+    NOP               ; No operation (takes 2 cycles)
+    DEY               ; Decrement inner loop counter
+    BNE INNER_LOOP    ; Branch if not zero
+    DEX               ; Decrement outer loop counter
+    BNE OUTER_LOOP    ; Branch if not zero
+    RTS               ; Return from subroutine
+
+;END--------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+;--------------------------------TIME MANAGEMENT------------------------------------
+;-----------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------
+
+
+
 nmi:
 irq:
     rti
