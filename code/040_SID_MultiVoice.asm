3f0bbebab775a92bb6d1f16e090ef6ebace11756
carlinhocr <carlos.rienzi@gmail.com>
2025-12-04 14:30:28 -0300

    040 Multivoice SID 1/16 Troubleshooting

--- code/040_SID_MultiVoice.asm
+++ code/040_SID_MultiVoice.asm
@@ -1070,7 +1070,7 @@ playExampleSong3Voices:
   ;lda #15 
   sta SID_FILTER_MV  
   ;set high pulse width voice 2 (SPECIFIC TO THIS SONG )
-  lda #8
+  lda #4
   sta SID_V2PWLH
   ;set high freq and low for filter cutt off (SPECIFIC TO THIS SONG)
   ;for all voices
@@ -1108,8 +1108,8 @@ playSIDMultiVoiceLoop:
   sta SID_V3FL
   lda (v1w_low),y
   sta SID_V1CTRL
-  ; lda (v2w_low),y
-  ; sta SID_V2CTRL
+  lda (v2w_low),y
+  sta SID_V2CTRL
   lda (v3w_low),y
   sta SID_V3CTRL
   
