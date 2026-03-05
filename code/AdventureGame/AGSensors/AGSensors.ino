695e58cf081f66970419a4eacc5e5add7dacb51c
carlinhocr <carlos.rienzi@gmail.com>
2026-03-05 12:31:17 -0300

    Heart Beat on per 6502 and relay

--- code/AdventureGame/AGSensors/AGSensors.ino
+++ code/AdventureGame/AGSensors/AGSensors.ino
@@ -6,15 +6,22 @@
 #define PB1  9
 #define PB2  10
 #define OUT1  11
-#define PWMOUT2  12
+#define OUT2  12
 
 int beatDelay = 50;
+int beatOnOFF=1; //off by default for a relay
 int relayDelay = 1000;
 
-void beat()
+void beatOn()
 {
-  beatDelay=55
-  analogWrite(PWMOUT2, beatDelay);
+//0 is using the relay to turn on
+  digitalWrite(OUT2, 0);
+}
+
+void beatOff()
+{
+//1 is using the relay to turn off
+  digitalWrite(OUT2, 1);
 }
 
 void relayOn()
@@ -27,15 +34,24 @@ void relayOff()
   digitalWrite(OUT1, 0);
 }
 
-// void onSync()
-// {
-//   Serial.print("Entra onSync: ");
-  //int bit0 = digitalRead(PB0) ? 1:0; //? ternary operator if TRUE then 1 else 0
+void onSync()
+{
+  Serial.print("Entra onSync: ");
+  int bit0 = digitalRead(PB0) ? 1:0; //? ternary operator if TRUE then 1 else 0
   //int bit1 = digitalRead(PB1) ? 1:0; //? ternary operator if TRUE then 1 else 0
   //int bit2 = digitalRead(PB2) ? 1:0; //? ternary operator if TRUE then 1 else 0
   //Serial.print(bit2);
   //Serial.print(bit1);
-  //Serial.println(bit0);
+  Serial.println(bit0);
+  if (bit0 == 1){
+    Serial.println("Activating HeartBeat Relay");
+    beatOn();  
+  }
+  else {
+    Serial.println("Turning Off HeartBeat Relay");
+    beatOff();
+  };
+}
   // if (bit2 == 1){
   //   Serial.println("Activating Relay");
   //   relayOn();
@@ -56,12 +72,12 @@ void relayOff()
   //   else if (bit1 == 1 and bit0 == 1){
   //   beatDelay = 200;
   // }
-}
+//}
  
 void setup()
 {
-  // pinMode(SYNC, INPUT);
-  // pinMode(PB0, INPUT);
+  pinMode(SYNC, INPUT);
+  pinMode(PB0, INPUT);
   // pinMode(PB1, INPUT);
   // pinMode(PB2, INPUT);
   pinMode(OUT1, OUTPUT);
@@ -74,7 +90,6 @@ void setup()
 }
 
 void loop() {
-  beat();
-  delay(5000);
+  delay(1000);
 }
 
