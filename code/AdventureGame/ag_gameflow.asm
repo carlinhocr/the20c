6d41b7e7243fafa0ae278fd6920eff637a6d28f5
carlinhocr <carlos.rienzi@gmail.com>
2026-03-06 14:11:11 -0300

    AG gameflow only screen

--- code/AdventureGame/ag_gameflow.asm
+++ code/AdventureGame/ag_gameflow.asm
@@ -735,9 +735,9 @@ load_screen_ram_end:
   rts
 
 draw_current_screen_table:
-  jsr draw_screen_ascii
+  ;jsr draw_screen_ascii
   ;jsr draw_screen_by_hand
-  jsr draw_screen_description_flashlight
+  ;jsr draw_screen_description_flashlight
   jsr draw_screen_by_ram_ascii  
 ;  jsr draw_screen_description
 ;  jsr selectPuzzle
