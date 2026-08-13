997d69106b9f677086be4d99bc4abaf81fb2b909
carlinhocr <carlos.rienzi@gmail.com>
2026-08-13 14:47:18 -0300

    Starting ASCII BANNER

---
+++ code/lib_20c/lib_ascii_banner/lib_ascii_banner_demo.asm
@@ -0,0 +1,31 @@
+  .include "../lib_acia/lib_acia_memory.asm" ;define memory address for ACIA
+  .include "../lib_acia/lib_acia_constants.asm" ;define constansts that are not memory addresses but literals for ACIA  
+
+
+
+
+
+
+  .org $8000
+  .include "../lib_init/lib_init.asm" ;reset vector and stack initialization
+
+
+;Plan to create an ASCII BANNER command
+
+;constants that have the ASCII letter as if they where blocks on a 5x8 character block
+;fucntion to read an ASCII character code and find the ASCII Letter block
+;function to read the encoded characters and copy them to RAM memory
+;function to read several ascii characters in the same Line and copy them to RAM
+;function to get ascii in RAM and print it to RS-232 (the ACIA module has this function)
+
+
+  .include "../lib_acia/lib_acia_code.asm" ;define code for ACIA t  
+
+
+;complete the file
+  .org $fffa
+  .word nmi ;a word is 16 bits or two bytes in this case $fffa and $fffb
+  .org $fffc ;go to memory address $fffc of the reset vector
+  .word RESET ;store in $FFFC & $FFFD the memory address of the RESET: label  00 80 ($8000 in little endian)
+  .org $fffe
+  .word irq ;a word is 16 bits or two bytes in this case $fffe and $ffff
