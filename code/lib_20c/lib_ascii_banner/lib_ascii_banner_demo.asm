704fb223c4a3a5fe8c2cbe57c8fe4b1478e9235e
carlinhocr <carlos.rienzi@gmail.com>
2026-09-12 14:45:28 -0300

    Testing String Slice

--- code/lib_20c/lib_ascii_banner/lib_ascii_banner_demo.asm
+++ code/lib_20c/lib_ascii_banner/lib_ascii_banner_demo.asm
@@ -52,21 +52,21 @@
   jsr printLineRAM
   jsr delay_1_sec
 
-  ; ;load the alphabet
-  ; lda #<asciiBannerAlphabet_c64
-  ; sta asciiBannerAlphabet_low
-  ; lda #>asciiBannerAlphabet_c64  
-  ; sta asciiBannerAlphabet_high
-  ; ;load the string
-  ; lda #<asciiStringShort
-  ; sta asciiLongStringZp_low
-  ; lda #>asciiStringShort
-  ; sta asciiLongStringZp_high
-  ; ;slice and print the string of lenght asciiPrintLenght
-  ; jsr sliceAsciiStrings
-  ; jsr delay_1_sec
-  ; jsr delay_1_sec
-  ; jsr delay_1_sec
+  ;load the alphabet
+  lda #<asciiBannerAlphabet_c64
+  sta asciiBannerAlphabet_low
+  lda #>asciiBannerAlphabet_c64  
+  sta asciiBannerAlphabet_high
+  ;load the string
+  lda #<asciiStringShort
+  sta asciiLongStringZp_low
+  lda #>asciiStringShort
+  sta asciiLongStringZp_high
+  ;slice and print the string of lenght asciiPrintLenght
+  jsr sliceAsciiStrings
+  jsr delay_1_sec
+  jsr delay_1_sec
+  jsr delay_1_sec
 
   ;load the alphabet
   lda #<asciiBannerAlphabet_c64
