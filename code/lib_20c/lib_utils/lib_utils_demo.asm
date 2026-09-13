7c7bbb1275a1bef1053766ebfdb59baa8586ecd8
carlinhocr <carlos.rienzi@gmail.com>
2026-09-13 19:59:33 -0300

    Move Memory

---
+++ code/lib_20c/lib_utils/lib_utils_demo.asm
@@ -0,0 +1,54 @@
+  .include "lib_utils_memory.asm" ;define memory address for ACIA
+
+  .include "../lib_acia/lib_acia_memory.asm" ;define memory address for ACIA  
+
+  .org $8000
+  .include "../lib_init/lib_init.asm" ;reset vector and stack initialization
+  
+  ;transfer any amount of bytes from one memory area to another
+  ;load how many bytes to transfer
+  ;example for 4 bytes
+  lda #$4
+  sta utilMemoryTransfer_LowByte
+  lda #$0
+  sta utilMemoryTransfer_HighByte
+;
+;example for 500 bytes = $01F4
+;lda #$F4
+;sta utilMemoryTransfer_LowByte
+;lda #$01
+;sta utilMemoryTransfer_HighByte
+;
+;now load the memory FROM, for example to copy from ROM $9000
+  lda #$00
+  sta utilPivot_01_ZP_low
+  lda #$90
+  sta utilPivot_01_ZP_high
+;now load the memory TO, for example to copy to RAM $1000
+  lda #$00
+  sta utilPivot_02_ZP_low
+  lda #$10
+  sta utilPivot_02_ZP_high
+;now run the transfer function
+  jsr memoryTransfer  
+
+loop:
+  jmp loop
+
+  .include "lib_utils_code.asm" ;define code for Utils
+  .include "../lib_acia/lib_acia_code.asm" ;define code for ACIA t  
+
+  .include "lib_utils_constants.asm" ;define constansts that are not memory addresses but literals for ACIA  
+  .include "../lib_acia/lib_acia_constants.asm" ;define constansts that are not memory addresses but literals for ACIA  
+
+nmi:
+irq:
+  rti
+
+;complete the file
+  .org $fffa
+  .word nmi ;a word is 16 bits or two bytes in this case $fffa and $fffb
+  .org $fffc ;go to memory address $fffc of the reset vector
+  .word RESET ;store in $FFFC & $FFFD the memory address of the RESET: label  00 80 ($8000 in little endian)
+  .org $fffe
+  .word irq ;a word is 16 bits or two bytes in this case $fffe and $ffff
