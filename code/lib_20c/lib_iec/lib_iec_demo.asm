1bbb76469e9f014cb2d39ba9884ce7241a8ea209
carlinhocr <carlos.rienzi@gmail.com>
2026-09-13 18:31:00 -0300

    Starting IEC library

---
+++ code/lib_20c/lib_iec/lib_iec_demo.asm
@@ -0,0 +1,15 @@
+;DEMO for the IEC library
+;
+;PART 1 SAVE FILE
+;define an area in memory (RAM or ROM)
+;define the number of bytes to copy (16 bits)
+;get FILENAME, DEVICE
+;save a file with that content to disk.
+;
+;PART 2 LOAD FILE
+;define an area in memory RAM
+;load the file to that area
+;
+;PART 3 DELETE A FILE
+;
+;
