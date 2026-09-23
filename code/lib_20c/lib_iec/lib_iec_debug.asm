0da94353c5455c14df720cf68f18e3c18d1c52c1
carlinhocr <carlos.rienzi@gmail.com>
2026-09-23 15:00:26 -0300

    Starting IEC Library

--- code/lib_20c/lib_iec/lib_iec_debug.asm
+++ code/lib_20c/lib_iec/lib_iec_debug.asm
@@ -14,7 +14,7 @@ debugIEC_transferToBuffer:
   ;example for 256 bytes
   lda #212
   sta utilMemoryTransfer_LowByte
-  lda #1
+  lda #2
   sta utilMemoryTransfer_HighByte
   ;now load the memory FROM, for example to copy from ROM $9000
   ;or modify it for string example
